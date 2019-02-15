9cdcb7e371acbb01738b966b5545d0e2d9290e77
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-02-15 11:09:08 -0600

    set up bare bones song list component and import it into index js file to make sure it is wired up correctly

---
+++ client/components/SongList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+	render() {
+		return ( 
+			<div>
+				SongList coming soon
+			</div>
+		);
+	}
+
+} 
+
+export default SongList;
