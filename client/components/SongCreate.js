2a787c35384450a6bb2adf27a7b73dea316e625a
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-02-19 12:06:29 -0600

    add a form that allows the user to input a song and updates state for each keystroke

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			title: ''
+		};
+	}
+
+	onSubmit(event){
+		event.preventDefault();
+
+		
+	}
 	render(){
 		return (
 			<div>
 				<h3> Create a new song </h3>
 				<form>
+					<label>Song Title:</label>
+					<input
+						onChange={event => this.setState({ title: event.target.value  })}
+						value={this.state.title }
+					/>
 				</form>
 			</div>
 		);
